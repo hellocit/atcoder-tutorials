e3b91302d4adc9294993e51105d4db89f93ed136
hellocit <bestoosaka@gmail.com>
2023-08-03 19:02:50 +0900

    finish else

---
+++ EX-problems/EX6_4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (void){
+    int x;
+    cin >> x;
+
+    if (x < 10){
+        cout << "xは10より小さい" << endl;
+        }
+    else {
+        cout << "xは10より小さくない" << endl;
+    }
+    return 0;
+}
